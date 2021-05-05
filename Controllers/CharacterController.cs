4cc61af1de686505275e81d167fcea9bc55c9c0d
zander <zander.riggs@gmail.com>
2021-05-05 11:55:38 -0400

    exploring options for dropdown lists. Attempt at Razor-style dropdown

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using NumeneraCharGen.Models;
-using System.Dynamic;
 
 namespace NumeneraCharGen.Controllers
 {
@@ -15,13 +14,27 @@ namespace NumeneraCharGen.Controllers
         // GET: Character
         public ActionResult Index()
         {
-            Character temp = new Character();
-
-            temp.DescriptorData = GetDescriptors();
-            temp.TypeData = GetTypes();
-            temp.FocusData = GetFoci();
+            Character temp = new Character()
+            {
+                DescriptorData = GetDescriptors(),
+                TypeData = GetTypes(),
+                FocusData = GetFoci()
+            };
 
             return View(temp);
+        }
+
+        // Generate Character
+        private ActionResult GenerateCharacter(int description_id, int type_id, int focus_id)
+        {
+            Character newCharacter = new Character()
+            {
+                DescriptorData = GetDescriptor(description_id),
+                TypeData = GetType(type_id),
+                FocusData = GetFocus(focus_id)
+            };
+
+            return View(newCharacter);
         }
 
         // GET: Character/Details/5
@@ -102,11 +115,25 @@ namespace NumeneraCharGen.Controllers
             var descriptors = db.Descriptors.OrderBy(x => x.Name);
             return descriptors.ToList();
         }
-        
+
+        // GET: Descriptor
+        private List<Descriptor> GetDescriptor(int descriptor_id)
+        {
+            var descriptor = db.Descriptors.Where(x => x.Descriptor_id == descriptor_id);
+            return descriptor.ToList();
+        }
+
         // GET: Types
         private List<Models.Type> GetTypes()
         {
             var types = db.Types.OrderBy(x => x.Name);
+            return types.ToList();
+        }
+
+        // GET: Type
+        private List<Models.Type> GetType(int type_id)
+        {
+            var types = db.Types.Where(x => x.Type_id == type_id);
             return types.ToList();
         }
 
@@ -116,5 +143,14 @@ namespace NumeneraCharGen.Controllers
             var focus = db.Foci.OrderBy(x => x.Name);
             return focus.ToList();
         }
+
+        // GET: Focus
+        private List<Focus> GetFocus(int focus_id)
+        {
+            var focus = db.Foci.OrderBy(x => x.Focus_id == focus_id);
+            return focus.ToList();
+        }
+
+
     }
 }
